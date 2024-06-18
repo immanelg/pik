af2c2921fe1b7e7d70215ff9b98c17e41860dea1
immanelg <119798691+immanelg@users.noreply.github.com>
2024-06-18 17:22:50 +0300

    feat: log file

--- app/cmd.go
+++ app/cmd.go
@@ -3,23 +3,38 @@ package app
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 )
 
 func Run() {
 	var initialColor string
+	var logfile string
 	flag.StringVar(&initialColor, "hex", "", "initial color to edit (hex)")
+	flag.StringVar(&logfile, "logfile", "", "file for debug logging")
 	flag.Parse()
 
-	var initialRgb rgb
+	var initialRgb rgb = initRgb
 	if initialColor != "" {
 		var err error
 		initialRgb, err = hexToRgb(initialColor)
 		if err != nil {
 			log.Fatalf("cannot parse HEX color: %s\n", err)
 		}
+	}
+
+	if logfile != "" {
+		f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer f.Close()
+
+		log.SetOutput(f)
+		log.Println("started")
 	} else {
-		initialRgb = blackRgb
+		log.SetOutput(io.Discard)
 	}
 
 	app := newApp(initialRgb)
